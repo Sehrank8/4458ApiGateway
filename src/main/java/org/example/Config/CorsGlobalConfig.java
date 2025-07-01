283c281044736b0b9ca1be527091612d54e531ce
Şehran <sehrankartal@gmail.com>
2025-07-01 20:56:19 +0300

    First commit

---
+++ src/main/java/org/example/Config/CorsGlobalConfig.java
@@ -0,0 +1,21 @@
+package org.example.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsGlobalConfig {
+    @Bean
+    public WebFluxConfigurer corsConfigurer() {
+        return new WebFluxConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
